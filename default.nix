dada9ef316f54151caf24e7fd6a51510822287d1
David Meister <thedavidmeister@gmail.com>
2019-06-27 19:42:06 +1000

    use rust nightly branch in nix

--- default.nix
+++ default.nix
@@ -2,11 +2,12 @@ let
  holonix-release-tag = "2019-06-27-rust-nightly";
  holonix-release-sha256 = "1n26n9q4i2k11n1m7disjs7s5s11lq29icqyk8qngqs5gf7kq4pi";
 
- hxolonix = import (fetchTarball {
+ holonix = import (fetchTarball {
   url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
   # sha256 = "${holonix-release-sha256}";
  });
- holonix = import ../holonix;
+ # uncomment to work locally
+ # holonix = import ../holonix;
 in
 with holonix.pkgs;
 {
