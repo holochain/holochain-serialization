b5a3beba9c3ade44356f00b26be90216bcbb3add
David Meister <thedavidmeister@gmail.com>
2019-06-27 19:36:07 +1000

    wip on nix + circle

---
+++ default.nix
@@ -0,0 +1,20 @@
+let
+ holonix-release-tag = "2019-06-27-rust-nightly";
+ holonix-release-sha256 = "1n26n9q4i2k11n1m7disjs7s5s11lq29icqyk8qngqs5gf7kq4pi";
+
+ hxolonix = import (fetchTarball {
+  url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
+  # sha256 = "${holonix-release-sha256}";
+ });
+ holonix = import ../holonix;
+in
+with holonix.pkgs;
+{
+ core-shell = stdenv.mkDerivation (holonix.shell // {
+  name = "holochain-serialization-shell";
+
+  buildInputs = []
+   ++ holonix.shell.buildInputs
+  ;
+ });
+}
