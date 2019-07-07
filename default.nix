1737b0321f5189c5aab1ac419e8b4a5e1c14058f
David Meister <thedavidmeister@gmail.com>
2019-07-07 15:05:23 +1000

    holonix 0.0.4

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
- holonix-release-tag = "2019-06-27-rust-nightly";
- holonix-release-sha256 = "1n26n9q4i2k11n1m7disjs7s5s11lq29icqyk8qngqs5gf7kq4pi";
+ holonix-release-tag = "0.0.4";
+ holonix-release-sha256 = "04xbhr0w0fc911vjd4f42shmy3vlnvwpicjnsk0q8gr4prkpz74h";
 
  holonix = import (fetchTarball {
   url = "https://github.com/holochain/holonix/tarball/${holonix-release-tag}";
-  # sha256 = "${holonix-release-sha256}";
+  sha256 = "${holonix-release-sha256}";
  });
  # uncomment to work locally
  # holonix = import ../holonix;
