af6d62242f573e11bb5f7bb233671799f90d6efa
David Meister <thedavidmeister@gmail.com>
2019-07-10 00:11:15 +1000

    Release 0.0.7

--- config.nix
+++ config.nix
@@ -62,7 +62,7 @@ hn-release-hook-publish-rust \
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "28eb999470dbbb9945322c760134895450877630";
+  commit = "819688a90d3836587b9e5d5c9722c6b9c7534df0";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
