827e849547fc9f0f10ffec1a7bf2cf9ea3b7c736
David Meister <thedavidmeister@gmail.com>
2019-07-09 23:39:41 +1000

    use regex for version bumping

--- release/hook/version/default.nix
+++ release/hook/version/default.nix
@@ -6,9 +6,10 @@ let
 echo "bumping holochain_json_derive dependency versions to ${config.release.version.current} in all Cargo.toml"
 find . \
  -name "Cargo.toml" \
+ -not -path "**/target/**" \
  -not -path "**/.git/**" \
  -not -path "**/.cargo/**" | xargs -I {} \
- sed -i 's/^\s*holochain_json_derive\s*=\s*{\s*version\s*=\s*"=\d+\.\d+\.\d+(-alpha\d*)?"/holochain_json_derive = {version = "=${config.release.version.current}"/g' {}
+ sed -i 's/^holochain_json_derive = { version = "=[0-9]\+.[0-9]\+.[0-9]\+\(-alpha[0-9]\+\)\?"/holochain_json_derive = { version = "=${config.release.version.current}"/g' {}
 '';
 in
 {
