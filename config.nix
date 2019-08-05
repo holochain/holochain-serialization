3e62f52004f4a2a2aacc88822272712ea6ececa0
Eric Harris-Braun <eric@harris-braun.com>
2019-08-04 22:24:28 -0400

    reverting to holonix 20 because 22 is broken with github-release

--- config.nix
+++ config.nix
@@ -12,14 +12,14 @@
 
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "0.0.22";
+   ref = "0.0.20";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "0f373k3c6318qgya6mhk199ins2a4g4rkbqy3v382nix990iq2va";
+   sha256 = "0k8d66dl9cwhikwl8a24gxald12lvbl4zf0jxfak9snpnb5mnlnm";
 
    # the github owner of the holonix repo
    owner = "holochain";
