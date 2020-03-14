f9e338c74513db98806c8b31230b5d3171cb0cf2
David Meister <thedavidmeister@gmail.com>
2020-03-14 21:22:10 +0400

    Release v0.0.28

--- config.nix
+++ config.nix
@@ -60,13 +60,13 @@ hcs-release-hook-version
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "766e56ce422eb7865268850cd4ef547ab27c294a";
+  commit = "80efae1622792a29019b600362e48fe7a683836f";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.26";
+   current = "0.0.28";
    # not used by version hooks in this repo
    previous = "_._._";
   };
