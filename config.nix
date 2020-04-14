5ca2dc61a06caab9951ad308d098989e865ee642
David Meister <thedavidmeister@gmail.com>
2020-04-14 22:31:50 +0400

    Release v0.0.37

--- config.nix
+++ config.nix
@@ -60,13 +60,13 @@ hn-release-hook-version-rust-deps 'holochain_json_derive holochain_serialized_by
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "b1feb1979cc5dfc3d12ec473e2eb3fb98876ae46";
+  commit = "b22fa2183bcfff728f236d7fc98fd909116b9e41";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.36";
+   current = "0.0.37";
    # not used by version hooks in this repo
    previous = "_._._";
   };
