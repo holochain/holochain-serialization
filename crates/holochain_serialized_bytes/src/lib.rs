da1d5e57453466545f681f956a0352c9e177d032
David Meister <thedavidmeister@gmail.com>
2020-06-06 21:01:16 +0400

    benches and serde_bytes

--- crates/holochain_serialized_bytes/src/lib.rs
+++ crates/holochain_serialized_bytes/src/lib.rs
@@ -85,7 +85,7 @@ impl From<SerializedBytes> for UnsafeBytes {
 /// - round tripping data through a database that has its own serialization preferences
 /// - debug output or logging of data that is to be human readible
 /// - moving between data types within a single system that has no external facing representation
-pub struct SerializedBytes(Vec<u8>);
+pub struct SerializedBytes(#[serde(with = "serde_bytes")] Vec<u8>);
 
 impl SerializedBytes {
     pub fn bytes(&self) -> &Vec<u8> {
@@ -340,8 +340,8 @@ pub mod tests {
                 inner: fixture_foo().try_into().unwrap()
             },
             vec![
-                129, 165, 105, 110, 110, 101, 114, 155, 204, 129, 204, 165, 105, 110, 110, 101,
-                114, 204, 163, 102, 111, 111
+                129, 165, 105, 110, 110, 101, 114, 196, 11, 129, 165, 105, 110, 110, 101, 114, 163,
+                102, 111, 111
             ]
         );
     }
