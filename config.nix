d06951c6f2f51509151020d080560a98347bd541
Michael Dougherty <maackle.d@gmail.com>
2020-12-22 11:52:24 -0500

    Roll back version number

--- config.nix
+++ config.nix
@@ -66,7 +66,7 @@ hn-release-hook-version-rust-deps 'holochain_json_derive holochain_serialized_by
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.47";
+   current = "0.0.46";
    # not used by version hooks in this repo
    previous = "_._._";
   };
