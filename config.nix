2043b7ce7c9b5a4dffccc2a57178047c69c6039a
thedavidmeister <thedavidmeister@gmail.com>
2021-02-21 22:27:26 +0400

    Release v0.0.50

--- config.nix
+++ config.nix
@@ -60,13 +60,13 @@ hn-release-hook-version-rust-deps 'holochain_json_derive holochain_serialized_by
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "1ad746670783f3766b1b3ec2f87bc7ff02628fb4";
+  commit = "f98e370ebb28bc9f6aca0577a0ac10e763a4c561";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.49";
+   current = "0.0.50";
    # not used by version hooks in this repo
    previous = "_._._";
   };
